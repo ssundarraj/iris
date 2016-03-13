dfe2be9a1233a42f05d31ba56e420e7bb958bf27
Sriram Sundarraj <ssundarraj@gmail.com>
2016-03-13 15:44:50 +0530

    Update to generate csv.

--- digit/digit.py
+++ digit/digit.py
@@ -4,8 +4,13 @@ from sklearn import metrics
 import csv
 
 
+train_X = []
+train_Y = []
+test_X = []
+test_Y = []
 X = []
 Y = []
+
 n_cases = 4000
 i = 0
 with open('dataset/train.csv', 'rb') as csvfile:
@@ -15,24 +20,24 @@ with open('dataset/train.csv', 'rb') as csvfile:
         r = [float(o) for o in r]
         Y.append(r[0])
         X.append(r[1:])
-        i += 1
-        if i >= n_cases:
-            break
+        # i += 1
+        # if i >= n_cases:
+        #     break
 
+with open('dataset/test.csv', 'rb') as csvfile:
+    digitdata = csv.reader(csvfile, delimiter=',')
+    next(digitdata, None)
+    for r in digitdata:
+        r = [float(o) for o in r]
+        test_Y.append(r[0])
+        test_X.append(r)
 
 X = preprocessing.scale(X)
-train_X = []
-train_Y = []
-test_X = []
-test_Y = []
+test_X = preprocessing.scale(test_X)
 
 for i in range(n_cases):
-    if i%4:
-        train_X.append(X[i])
-        train_Y.append(Y[i])
-    else:
-        test_X.append(X[i])
-        test_Y.append(Y[i])
+    train_X.append(X[i])
+    train_Y.append(Y[i])
 
 print "learning cases {}".format(len(X))
 print "test cases {}".format(len(test_X))
@@ -42,10 +47,13 @@ clf.fit(X, Y)
 
 print clf
 
-n_infractions = 0
-
 preds = clf.predict(test_X)
-accuracy_score = metrics.accuracy_score(preds, test_Y)
+with open('dataset/submission.csv', 'w') as opfile:
+    for pred in preds:
+        pred = int(pred)
+        print pred
+        opfile.write(str(pred))
 
-print "incorrect predictions {}".format(n_infractions)
-print "accuracy {}".format(accuracy_score)
+# accuracy_score = metrics.accuracy_score(preds, test_Y)
+# print "accuracy {}".format(accuracy_score)
+
